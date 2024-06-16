3f20976fdf3413310444370567b67476626c0b68
v_litongxin01 <v_litongxin01@baidu.com>
2024-06-16 22:07:30 +0800

    完成 diff 算法

--- src/lib/reconclier/ReactChildFiber.js
+++ src/lib/reconclier/ReactChildFiber.js
@@ -1,5 +1,10 @@
-import { isStr, isArray } from "../shared/util";
+import { isStr, isArray, Update } from "../shared/util";
 import createFiber from "./ReactFiber";
+import {
+    sameNode, placeChild,
+    deleteRemainingChildren,
+    mapRemainingChildren,
+} from './ReactChildFiberAssistant';
 
 export function reconcileChildren(returnFiber, children) {
     // 如果 children 是一个字符串，那么说明这是一个文本节点
@@ -21,8 +26,88 @@ export function reconcileChildren(returnFiber, children) {
     // true 代表组件更新
     // false 代表组件初次渲染
     let shouldTrackSideEffects = !!returnFiber.alternate;
+    // 该变量有两个作用：
+    // 1. 存储下一个旧的 fiber 对象
+    // 2. 临时存储当前的旧的 fiber 对象
+    let nextOldFiber = null;
+
+
+    // 接下来就是我们整个 diff 核心的算法思想：整体分为 5 个步骤
+    // 1. 第一轮遍历，从左往右遍历新节点（vnode），在遍历的同时比较新旧节点（旧节点是 fiber 对象）
+    //  如果节点可以复用，那么复用，循环继续往右边走
+    //  如果节点不能够复用，那么就跳出循环，结束第一轮遍历
+    // 2. 检查 newChildren 是否完成了遍历，因为从上面第一步出来，就两种：
+    //  要么是提前跳出来的
+    //  要么是遍历完了跳出来，如果新节点完成了整个遍历，但是旧节点（fiber对象）还存在，那么就将旧节点删除
+    // 3. 初次渲染（这一步我们其实之前已经完成了）
+    //  还有一种情况也是属于初次渲染：旧节点遍历完了，新节点还有剩余，那么这些新节点就是属于初次渲染
+    // 4. 处理新旧节点都还有剩余的情况
+    // （1）将剩下旧节点放入到一个 map 结构里面，方便之后使用
+    // （2）遍历剩余的新节点，通过新节点的 key 去 map 里面进行查找，看有没有能够复用的旧节点，如果有，拿来复用，并且会从 map 中删除对应的旧节点
+    // 5. 整个新节点遍历完成后，如果 map 中还有剩余的旧节点，这些旧节点也就没有用了，直接删除即可
+
     // // 第一轮遍历，会尝试复用节点
     for (; oldFiber && i < newChildren.length; i++) {
+        // 当前的 VNode 对象
+        const newChild = newChildren[i];
+
+        if (!newChild) {
+            continue;
+        }
+
+        // 在判断是否能够复用之前，我们先给 nextOldFiber 赋值
+        // 这里有一种情况
+        // old 一开始是 1 2 3 4 5，进行了一些修改，现在只剩下 5 和 4
+        // old >> 5(4) 4(3)
+        // new >> 4(3) 1 2 3 5(4)
+        // 此时旧的节点的 index 是大于 i，因此我们需要将 nextOldFiber 暂存为 oldFiber
+        if (oldFiber.index > i) {
+            nextOldFiber = oldFiber;
+            oldFiber = null
+        } else {
+            nextOldFiber = oldFiber.sibling;
+        }
+
+        // 判断是否能够复用
+        const same = sameNode(newChild, oldFiber);
+
+        if (!same) {
+            // 说明不能复用，跳出循环
+            // 我们需要将 oldFiber 原本的值还原，方便后面使用
+            oldFiber = nextOldFiber;
+            break;
+        }
+
+        // 说明可以复用
+        const newFiber = createFiber(newChild, returnFiber);
+         // 复用旧 fiber 上面的部分信息，特别是 DOM 节点
+        Object.assign(newFiber, {
+            stateNode: oldFiber.stateNode,
+            alternate: oldFiber,
+            flags: Update,
+        });
+
+        // 更新 lastPlacedIndex 的值
+        lastplacedIndex = placeChild(
+            newFiber,
+            lastplacedIndex,
+            i,
+            shouldTrackSideEffects
+        );
+
+        // 将新生成的 fiber 加入到 fiber 链表里面去
+        if (previousFiber === null) {
+            // 说明是第一个子节点
+            returnFiber.child = newFiber;
+        } else {
+            // 当前生成的 fiber 节点并非父 fiber 的第一个节点
+            previousFiber.sibling = newFiber;
+        }
+
+        // 将 previousFiber 设置为 newFiber
+        previousFiber = newFiber;
+        // oldFiber 存储下一个旧节点信息
+        oldFiber = nextOldFiber;
 
     }
 
@@ -31,11 +116,15 @@ export function reconcileChildren(returnFiber, children) {
     // 2. i === newChildren.length 说明是更新
 
     if (i === newChildren.length) {
-        // 说明是更新，需要删除多余的节点
+        // 如果还有剩余节点，需要删除多余的节点
+        deleteRemainingChildren(returnFiber, oldFiber);
+        return;
     }
 
     // 说明是初次渲染，需要创建新的节点
     if (!oldFiber){
+        // 需要将 newChildren 数组中的每一个元素都生成一个 fiber 对象
+        // 然后将这些 fiber 对象串联起来
         for (; i < newChildren.length; i++) {
             const newChildVnode = newChildren[i];
 
@@ -62,23 +151,60 @@ export function reconcileChildren(returnFiber, children) {
             previousFiber = newFiber;
         }
 
+        // 处理新旧节点都还有剩余的情况
+        // 首先创建一个 map 结构，用于存储剩余的旧节点
+
+        const existingChildren = mapRemainingChildren(oldFiber);
+
+        for (; i < newChildren.length; i++) {
+            const newChild = newChildren[i];
+
+            if (newChild == null) {
+                return;
+            }
+
+            // 根据新节点的 vnode 去生成新的 fiber
+            const newFiber = createFiber(newChild, returnFiber);
+
+            // 接下来就需要去哈希表里面寻找是否有可以复用的节点
+            const matchedFiber = existingChildren.get(newFiber.key || newFiber.index);
+
+            if (matchedFiber) {
+                // 说明可以复用
+                Object.assign(newFiber, {
+                    stateNode: matchedFiber.stateNode,
+                    alternate: matchedFiber,
+                    flags: Update,
+                });
+
+                // 更新 lastPlacedIndex 的值
+                lastplacedIndex = placeChild(
+                    newFiber,
+                    lastplacedIndex,
+                    i,
+                    shouldTrackSideEffects
+                );
+
+                // 将新生成的 fiber 加入到 fiber 链表里面去
+                if (previousFiber === null) {
+                    // 说明是第一个子节点
+                    returnFiber.child = newFiber;
+                } else {
+                    // 当前生成的 fiber 节点并非父 fiber 的第一个节点
+                    previousFiber.sibling = newFiber;
+                }
+                // 将 previousFiber 设置为 newFiber
+                previousFiber = newFiber;
+
+                // 整个新节点遍历完成后，如果 map 中还有剩余的旧节点，这些旧节点也就没有用了，直接删除即可
+                if (shouldTrackSideEffects) {
+                    existingChildren.forEach((child) => {
+                        deleteRemainingChildren(returnFiber, child);
+                    });
+                }
+            }
+        }
         
-    }
-
-}
-
-/**
- * 该方法专门用于更新 lastPlacedIndex
- * @param {*} newFiber 上面刚刚创建的新的 fiber 对象
- * @param {*} lastPlacedIndex 上一次的 lastPlacedIndex，也就是上一次插入的最远位置，初始值是 0
- * @param {*} newIndex 当前的下标，初始值也是 0
- * @param {*} shouldTrackSideEffects 用于判断 returnFiber 是初次渲染还是更新
- * @returns 
- */
-function placeChild(newFiber, lastPlacedIndex, newIndex, shouldTrackSideEffects) {
-    newFiber.index = newIndex;
-    if (!shouldTrackSideEffects) {
-        return lastPlacedIndex;
     }
 
 }
---
+++ src/lib/reconclier/ReactChildFiberAssistant.js
@@ -0,0 +1,101 @@
+import { Placement } from "../shared/util";
+
+/**
+ * 判断节点是否为相同
+ * 判断标准
+ *  1. 同一层级下面
+ *  2. 类型相同
+ *  3. key 相同
+ * @param {*} a 新的 vnode 节点
+ * @param {*} b 旧的 fiber 节点
+ */
+export function sameNode(a, b) {
+    return a && b && a.type === b.type && a.key === b.key;
+}
+
+
+/**
+ * 该方法专门用于更新 lastPlacedIndex
+ * @param {*} newFiber 上面刚刚创建的新的 fiber 对象
+ * @param {*} lastPlacedIndex 上一次的 lastPlacedIndex，也就是上一次插入的最远位置，初始值是 0
+ * @param {*} newIndex 当前的下标，初始值也是 0
+ * @param {*} shouldTrackSideEffects 用于判断 returnFiber 是初次渲染还是更新
+ * @returns 
+ */
+export function placeChild(newFiber, lastPlacedIndex, newIndex, shouldTrackSideEffects) {
+    // 更新 fiber 对象上面的 index
+    // fiber 对象上面的 index 记录当前 fiber 节点在当前层级下的位置
+    newFiber.index = newIndex;
+    if (!shouldTrackSideEffects) {
+        // 首次渲染，不需要记录节点位置
+        return lastPlacedIndex;
+    }
+
+    // 旧的 fiber
+    const current = newFiber.alternate;
+
+    if (current) {
+        // 旧的 fiber index
+        const oldIndex = current.index;
+        if (oldIndex < lastPlacedIndex) {
+            newFiber.flags |= Placement;
+
+            return lastPlacedIndex;
+        } else {
+            // // 进入此分支，说明 oldIndex 应该作为最新的 lastPlacedIndex
+            return oldIndex;
+        }
+    }
+    else {
+        // 进入此分支，说明当前的 fiber 是初次渲染
+        newFiber.flags |= Placement;
+        return lastPlacedIndex;
+    }
+
+}
+
+/**
+ * 
+ * @param {*} returnFiber 父节点 fiber 对象
+ * @param {*} childToDelete 待删除的节点 fiber 对象
+ */
+function deleteChild(returnFiber, childToDelete) {
+    // 这里的删除其实仅仅只是标记一下，真正的删除是在 commit 阶段
+    // 将要删除的 fiber 对象放入到到一个数组里面
+    const deletions = returnFiber.deletions;
+    if (deletions) {
+        returnFiber.deletions.push(childToDelete)
+    } else {
+        returnFiber.deletions = [childToDelete];
+    }
+}
+
+/**
+ * 删除多余节点
+ * @param {*} returnFiber 父节点 fiber 对象
+ * @param {*} currentFirstChild 待删除的第一个节点
+ */
+
+export function deleteRemainingChildren(returnFiber, currentFirstChild) {
+    let childToDelete = currentFirstChild;
+    while (childToDelete) {
+        // 删除节点
+        deleteChild(returnFiber, childToDelete);
+        childToDelete = childToDelete.sibling;
+    }
+}
+
+/**
+ * 将旧的子节点构建到一个 map 结构里面
+ * @param {*} currentFirstChild 
+ */
+export function mapRemainingChildren(currentFirstChild) {
+    const existingChildren = new Map();
+    let existingChild = currentFirstChild;
+    while (existingChild) {
+        existingChildren.set(existingChild.key || existingChild.index, existingChild);
+        existingChild = existingChild.sibling;
+    }
+
+    return existingChildren;
+}
